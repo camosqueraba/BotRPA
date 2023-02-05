0cf1a6f7d52693c42bcdd38204fd7195eb277c4f
Carlos Mosquera <carlos.mosquera@dogeniuson.com>
2023-02-05 13:20:08 -0500

    Add project files.

---
+++ BotRPA/Bot.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotRPA
+{
+    class Bot
+    {
+        static void Main(string[] args)
+        {
+            const string URL = "https://github.com/";
+            var driver = new ChromeDriver();
+            IJavaScriptExecutor jse = (IJavaScriptExecutor)driver;
+
+            driver.Manage().Window.Maximize();
+
+
+            var instrucciones = new Functionalities(driver, URL);
+
+            var descripcion = instrucciones.GetDescription();
+
+            jse.ExecuteScript($"alert('{descripcion}');");
+
+
+
+            //driver.Quit();
+        }
+    }
+}
---
+++ BotRPA/Functionalities.cs
@@ -0,0 +1,59 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotRPA
+{
+    public class Functionalities
+    {
+        private readonly ChromeDriver driver;
+        private readonly string URL;
+
+        public Functionalities(ChromeDriver driver, string URL)
+        {
+            this.driver = driver;
+            this.URL = URL;
+        }
+
+        public string GetDescription()
+        {
+            driver.Navigate().GoToUrl(URL);
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            IWebElement searchBar = wait.Until(e => e.FindElement(By.Name("q")));
+
+
+            searchBar = driver.FindElement(By.Name("q"));
+            searchBar.Click();
+            searchBar.SendKeys("selenium");
+            searchBar.SendKeys(Keys.Enter);
+
+            IWebElement result = wait.Until(e => e.FindElement(By.CssSelector(".repo-list-item:nth-child(4) a")));
+            result.Click();
+
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+
+
+
+            var layoutSidebar = new WebDriverWait(driver, TimeSpan.FromSeconds(10)).Until(drv => drv.FindElement(By.CssSelector(".Layout-sidebar")));
+
+
+            var stringDescription = layoutSidebar.FindElement(By.CssSelector(".BorderGrid.BorderGrid--spacious p")).Text;
+
+
+
+            Console.WriteLine("Elemento: {0}", stringDescription);
+
+
+
+            return stringDescription;
+
+        }
+    }
+}
